f10d31f6b626b2aa2406b5f0cca923ec6afbbfd4
Dominique <dlockett@wustl.edu>
2023-04-26 18:29:22 -0500

    work on iframes
    stuff

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// Collapsible elements
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -13,50 +14,101 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-   function disableContextMenuInIframe() {
-          const iframe = document.getElementById("my-iframe");
-          iframe.contentWindow.document.addEventListener("contextmenu", (event) => {
-              event.preventDefault();
-          });
-      }
+// Disable context menu in iframe
+function disableContextMenuInIframe() {
+  const iframe = document.getElementById("my-iframe");
+  iframe.contentWindow.document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
+}
 
+// Syntax highlighting
 if (window.hljs) {
-  hljs.configure({languages: []});
+  hljs.configure({ languages: [] });
   hljs.initHighlightingOnLoad();
   if (document.readyState && document.readyState === "complete") {
-    window.setTimeout(function() { hljs.initHighlighting(); }, 0);
+    window.setTimeout(function () {
+      hljs.initHighlighting();
+    }, 0);
   }
 }
 
-// manage active state of menu based on current page
+// Manage active state of menu based on current page
 $(document).ready(function () {
-  // active menu anchor
-  href = window.location.pathname
-  href = href.substr(href.lastIndexOf('/') + 1)
-  if (href === "")
-    href = "index.html";
+  // Active menu anchor
+  href = window.location.pathname;
+  href = href.substr(href.lastIndexOf("/") + 1);
+  if (href === "") href = "index.html";
   var menuAnchor = $('a[href="' + href + '"]');
 
-  // mark the anchor link active (and if it's in a dropdown, also mark that active)
-  var dropdown = menuAnchor.closest('li.dropdown');
-  if (window.bootstrap) { // Bootstrap 4+
-    menuAnchor.addClass('active');
-    dropdown.find('> .dropdown-toggle').addClass('active');
-  } else { // Bootstrap 3
-    menuAnchor.parent().addClass('active');
-    dropdown.addClass('active');
+  // Mark the anchor link active (and if it's in a dropdown, also mark that active)
+  var dropdown = menuAnchor.closest("li.dropdown");
+  if (window.bootstrap) {
+    // Bootstrap 4+
+    menuAnchor.addClass("active");
+    dropdown.find("> .dropdown-toggle").addClass("active");
+  } else {
+    // Bootstrap 3
+    menuAnchor.parent().addClass("active");
+    dropdown.addClass("active");
   }
 
   // Navbar adjustments
   var navHeight = $(".navbar").first().height() + 15;
-  var style = document.createElement('style');
+  var style = document.createElement("style");
   var pt = "padding-top: " + navHeight + "px; ";
   var mt = "margin-top: -" + navHeight + "px; ";
   var css = "";
-  // offset scroll position for anchor links (for fixed navbar)
+  // Offset scroll position for anchor links (for fixed navbar)
   for (var i = 1; i <= 6; i++) {
     css += ".section h" + i + "{ " + pt + mt + "}\n";
   }
   style.innerHTML = "body {" + pt + "padding-bottom: 40px; }\n" + css;
   document.head.appendChild(style);
+
+  // Lazy loading for embedded files
+  const embedContainers = document.querySelectorAll(".embed-container");
+
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function loadEmbed(element) {
+    const src = element.getAttribute("data-src");
+    const type = element.getAttribute("data-type");
+
+    if (type === "html") {
+      const iframe = document.createElement("iframe");
+      iframe.src = src;
+      element.appendChild(iframe);
+    } else if (type === "pdf") {
+      PDFObject.embed(src, element);
+    }
+
+    element.setAttribute("data-loaded", "true");
+  }
+
+  function checkEmbeds() {
+    embedContainers.forEach((container) => {
+      if (isInViewport(container) && !container.getAttribute("data-loaded")) {
+        loadEmbed(container);
+      }
+    });
+  }
+
+  checkEmbeds();
+
+  window.addEventListener("scroll", function () {
+    checkEmbeds();
+  });
+
+  window.addEventListener("resize", function () {
+    checkEmbeds();
+  });
 });
